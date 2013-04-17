854b3462522951c1a93b38ce78fbdff903b3e8a3
Greg Leuch <contact@gleuch.com>
2013-04-16 23:38:15 -0400

    a bit smarter puts for errors.

--- app.rb
+++ app.rb
@@ -28,15 +28,14 @@ _heading('Twitter Retweet Search')
 n = 0
 ['★RETWEET★IF★YOU★WANT★MORE★FOLLOWERS★'].each do |q|
   begin
-    recent = Tweet.where('LOWER(query) = ?', q.downcase).order('tweet_id desc').first rescue nil
+    recent = Tweet.where('LOWER(query) = ?', q.downcase).order('tweet_id desc').first# rescue nil
     puts "since: #{recent && recent.tweet_id}"
     Twitter.search(q, :since_id => (recent && recent.tweet_id), :result_type => 'recent', :rpp => 100).results.map do |status|
-      _debug("@#{status.from_user_id}: #{status.text}", 3)
+      _debug("@#{status.from_user}: #{status.text}", 3)
       tweet = status[:text]
       status.urls.each{|u| tweet.gsub!(u.url, u.expanded_url)}
       t = Tweet.create(:query => q.downcase, :tweet_id => status.id, :tweet => tweet, :tweet_created_on => status.created_at, :user_id => status.user.id, :name => status.user.name, :screen_name => status.user.screen_name, :profile_image_url => status.user.profile_image_url)
-      # puts t.inspect
-      # puts t.errors.inspect
+      puts t.errors.message.inspect if t.new_record?
       n += 1
     end
   rescue => err
